113cc52064baadcd2149c75992cab7b0630f7835
simon-bee <simonbee1303@gmail.com>
2019-06-24 13:01:38 +0200

    first commit

---
+++ readme.md
@@ -0,0 +1,25 @@
+# Challenge caniculaire du 24/06/2019 au 02/07/2019 #
+
+Intégration du site bière en MVC, avec le blog en supplément.
+
+## Liste des maquettes ##
+
+![maquette mobile index][/assets/img/MobileIndex.png]
+![maquette mobile posts][/assets/img/MobilePosts.png]
+![maquette mobile categories][/assets/img/MobileCateg.png]
+![maquette mobile beers][/assets/img/MobileBeers.png]
+![maquette large index][/assets/img/LargeIndex.png]
+![maquette large posts][/assets/img/LargePosts.png]
+![maquette large categories][/assets/img/LargeCateg.png]
+![maquette large beers][/assets/img/LargeBeers.png]
+
+## Mes objectifs ##
+
+- Création de l'interface graphique
+- Adaptation MVC du site bière sur cette nouvelle interface
+- Création d'un blog sur ce site
+- Gérer inscription/connexion
+- Gérer partie e-commerce
+- Implentation interface utilisateur
+- Implentation interface administrateur
+- ...
---
+++ www/views/layout/default.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Site</title>
+    <link rel="stylesheet" href="/assets/css/styles.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+</head>
+<body>
+    <header>
+        <div class="header-top">
+            <div class="title">
+                <h1>Site Biere</h1>
+            </div>
+
+            <div class="menu">
+                <input type="checkbox" class="burger">
+                <nav>
+                    <ul>
+                        <li><a href="/">Home</a></li>
+                        <li><a href="/">Categories</a></li>
+                        <li><a href="/">Beers</a></li>
+                        <li><a href="/">Order</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+
+        <div class="header-img">
+            <figure>
+            <img src="/assets/img/banner.jpg" alt="random pic">
+                <figcaption>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, veritatis labore esse similique ea explicabo fuga ullam nisi nam harum perferendis amet obcaecati reprehenderit sunt quidem exercitationem dolore qui deserunt?</figcaption>
+            </figure>
+        </div>
+    </header>
+
+    <main>
+        <section class="index">
+            <div class="index-demo-posts">
+                <h2>Subtitle</h2>
+                <div class="content">
+                    <article class="one-post">
+                        <img src="https://picsum.photos/100?random=1" alt="random pic">
+                        <div class="content-article">
+                            <h3>Blablabla</h3>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati culpa, deleniti quibusdam voluptatum expedita fuga. Excepturi id adipisci quasi inventore delectus facere ex perspiciatis, numquam suscipit? In officia dolorem iure.</p>
+                            <p><a href="#">Read more</a></p>
+                        </div>
+                    </article>
+                    <article class="one-post">
+                        <img src="https://picsum.photos/100?random=2" alt="random pic">
+                        <div class="content-article">
+                            <h3>Blablabla</h3>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati culpa, deleniti quibusdam voluptatum expedita fuga. Excepturi id adipisci quasi inventore delectus facere ex perspiciatis, numquam suscipit? In officia dolorem iure.</p>
+                            <p><a href="#">Read more</a></p>
+                        </div>
+                    </article>
+                    <article class="one-post">
+                        <img src="https://picsum.photos/100?random=3" alt="random pic">
+                        <div class="content-article">
+                            <h3>Blablabla</h3>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati culpa, deleniti quibusdam voluptatum expedita fuga. Excepturi id adipisci quasi inventore delectus facere ex perspiciatis, numquam suscipit? In officia dolorem iure.</p>
+                            <p><a href="#">Read more</a></p>
+                        </div>
+                    </article>
+                </div>
+            </div>
+
+            <div class="index-demo-beers">
+                <h2>Subtitle</h2>
+                <div class="content">
+                    <article class="one-post">
+                        <img src="https://picsum.photos/100?random=1" alt="random pic">
+                        <div class="content-article">
+                            <h3>Blablabla</h3>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati culpa, deleniti quibusdam voluptatum expedita fuga. Excepturi id adipisci quasi inventore delectus facere ex perspiciatis, numquam suscipit? In officia dolorem iure.</p>
+                            <p><a href="#">Read more</a></p>
+                        </div>
+                    </article>
+                    <article class="one-post">
+                        <img src="https://picsum.photos/100?random=2" alt="random pic">
+                        <div class="content-article">
+                            <h3>Blablabla</h3>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati culpa, deleniti quibusdam voluptatum expedita fuga. Excepturi id adipisci quasi inventore delectus facere ex perspiciatis, numquam suscipit? In officia dolorem iure.</p>
+                            <p><a href="#">Read more</a></p>
+                        </div>
+                    </article>
+                    <article class="one-post">
+                        <img src="https://picsum.photos/100?random=3" alt="random pic">
+                        <div class="content-article">
+                            <h3>Blablabla</h3>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati culpa, deleniti quibusdam voluptatum expedita fuga. Excepturi id adipisci quasi inventore delectus facere ex perspiciatis, numquam suscipit? In officia dolorem iure.</p>
+                            <p><a href="#">Read more</a></p>
+                        </div>
+                    </article>
+                </div>
+            </div>
+
+            <div class="subscribe">
+                <h2>Subscibe</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi quisquam natus, atque omnis deserunt dicta cupiditate tempora aspernatur excepturi voluptatibus magnam eum quas? Exercitationem, ratione? Neque repudiandae dolores deleniti voluptatibus?</p>
+                <form action="#" method="post">
+                    <input type="text" name="subscribe" value=""><button type="submit">Send</button>
+                </form>
+            </div>
+        </section>
+    </main>
+
+    <footer>
+        <div class="social">
+            <i class="fab fa-facebook-square"></i><i class="fab fa-twitter-square"></i><i class="fab fa-instagram"></i><i class="fab fa-pinterest-square"></i><i class="fab fa-google-plus-square"></i>
+        </div>
+        <p class="footer">
+            © 2019 SiteBiere // Allrights reserved
+        </p>
+        <p><span class="signed">by SKYMON</span></p>
+
+    </footer>
+</body>
+</html>
